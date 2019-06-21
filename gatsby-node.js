15298f49f7ac7149f1b3fa694ec84fe483cd7a91
Gordon Lam <gordon.lam@evolvingsolutions.com.au>
2019-06-21 15:03:20 +0930

    Fix identifier field

--- gatsby-node.js
+++ gatsby-node.js
@@ -41,22 +41,24 @@ exports.createPages = ({ actions, graphql }) => {
 
     postOrPage.forEach(edge => {
       let component, pathName;
-      if (edge.node.frontmatter.templateKey === "home-page") {
-        pathName = "/";
-        component = path.resolve(`src/pages/index.js`);
-      } else {
-        pathName = edge.node.frontmatter.path || edge.node.fields.slug;
-        component = path.resolve(`src/templates/${String(edge.node.frontmatter.templateKey)}.js`);
+      if(edge.node.frontmatter.templateKey!= null) {
+        if (edge.node.frontmatter.templateKey === "home-page") {
+          pathName = "/";
+          component = path.resolve(`src/pages/index.js`);
+        } else {
+          pathName = edge.node.frontmatter.path || edge.node.fields.slug;
+          component = path.resolve(`src/templates/${String(edge.node.frontmatter.templateKey)}.js`);
+        }
+        const id = edge.node.id;
+        createPage({
+          path: pathName,
+          component,
+          // additional data can be passed via context
+          context: {
+            id,
+          },
+        });
       }
-      const id = edge.node.id;
-      createPage({
-        path: pathName,
-        component,
-        // additional data can be passed via context
-        context: {
-          id,
-        },
-      });
     });
   });
 };
---
+++ src/templates/category.js
@@ -0,0 +1,14 @@
+
+class CategoryTemplate extends Component {
+  render() {
+    return (
+    );
+  }
+}
+
+CategoryTemplate.propTypes = {
+  category: PropTypes.shape({
+  }),
+};
+
+export default CategoryTemplate;
